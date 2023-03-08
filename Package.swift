d3cc562cfd8ae1f37f199c52ef05a74fbca3c04d
Tristan Warner-Smith <tristan@reveri.com>
2023-03-08 22:43:17 +0000

    Updates to use firebase sdk mirror too

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     products: [.library(name: "SegmentFirebase", targets: ["SegmentFirebase"])],
     dependencies: [
       .package(url: "https://github.com/segmentio/analytics-ios.git", exact: "4.1.8"),
-      .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "10.2.0")
+      .package(url: "https://github.com/reveri-health/firebase-ios-sdk-xcframeworks", exact: "10.2.0")
     ],
     targets: [
         .target(
             name: "SegmentFirebase",
             dependencies: [
                 .product(name: "Segment", package: "analytics-ios"),
-                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk")
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk-xcframeworks")
             ],
             path: "Segment-Firebase/Classes",
             publicHeadersPath: ""
