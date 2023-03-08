c6f64cec18870af31b5cc179709ce156cca660a2
Tristan Warner-Smith <tristan@reveri.com>
2023-03-08 22:48:31 +0000

    Changes package to use branch to allow named package

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     products: [.library(name: "SegmentFirebase", targets: ["SegmentFirebase"])],
     dependencies: [
       .package(url: "https://github.com/segmentio/analytics-ios.git", exact: "4.1.8"),
-      .package(url: "https://github.com/reveri-health/firebase-ios-sdk-xcframeworks", exact: "10.2.0")
+      .package(name: "firebase-ios-sdk", url: "https://github.com/reveri-health/firebase-ios-sdk-xcframeworks", branch: "release/10.2.0")
     ],
     targets: [
         .target(
             name: "SegmentFirebase",
             dependencies: [
                 .product(name: "Segment", package: "analytics-ios"),
-                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk-xcframeworks")
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk")
             ],
             path: "Segment-Firebase/Classes",
             publicHeadersPath: ""
